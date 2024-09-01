7a4de72b86849e995e02f398c0ed05fa80079474
Jales Fonseca <jalesfonsecaneto@gmail.com>
2024-09-01 02:10:21 -0300

    Pequeno projeto onde estudo um pouco dos conceitos elementares e fundamentais da construções de Web Api's usando arquitetura REST com .NET, além do ORM EntityFramework.

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,134 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            try
+            {
+                var produtos = _context.Produtos.AsNoTracking().ToList();
+
+                if (produtos is null)
+                {
+                    return NotFound("Produtos não encontrados");
+                }
+
+                return produtos;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um erro ao tentar tratar sua solicitação.");
+            }
+            
+        }
+
+        [HttpGet("{id:int}", Name ="ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            try
+            {
+                var produto = _context.Produtos.AsNoTracking().FirstOrDefault(i => i.ProdutoId == id);
+
+                if (produto is null)
+                {
+                    return NotFound($"Produto com id={id} não encontrado");
+                }
+
+                return produto;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um erro ao tentar tratar sua solicitação.");
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto) 
+        {
+            try
+            {
+                if (produto is null)
+                {
+                    return BadRequest("Dados inválidos.");
+                }
+
+                _context.Produtos.Add(produto);
+                _context.SaveChanges();
+
+                return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um erro ao tentar tratar sua solicitação.");
+            }
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            try
+            {
+                if (id != produto.ProdutoId)
+                {
+                    return BadRequest("Dados inválidos.");
+                }
+
+                _context.Entry(produto).State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return Ok(produto);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um erro ao tentar tratar sua solicitação.");
+            }
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<Produto> Delete(int id)
+        {
+            try
+            {
+                var produto = _context.Produtos.FirstOrDefault(i => i.ProdutoId == id);
+
+                if (produto is null)
+                {
+                    return BadRequest($"Produto com id={id} não encontrado");
+                }
+
+                _context.Produtos.Remove(produto);
+                _context.SaveChanges();
+
+                return Ok(produto);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um erro ao tentar tratar sua solicitação.");
+            }
+        }
+    }
+}
---
+++ Migrations/20240831030827_PopulaProdutos.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO PRODUTOS(NOME, DESCRICAO, PRECO, " +
+                "IMAGEMURL, ESTOQUE, DATACADASTRO, CATEGORIAID) VALUES ('Coca Cola Diet'," +
+                " 'Refrigerante de Cola 350 mL', 5.45, 'cocacola.jpg', 50, now(), 1)");
+
+            migrationBuilder.Sql("INSERT INTO PRODUTOS(NOME, DESCRICAO, PRECO, " +
+               "IMAGEMURL, ESTOQUE, DATACADASTRO, CATEGORIAID) VALUES ('Lache de Atum'," +
+               " 'Lanche de Atum com maionese', 8.50, 'lanchedeatum.jpg', 50, now(), 2)");
+
+            migrationBuilder.Sql("INSERT INTO PRODUTOS(NOME, DESCRICAO, PRECO, " +
+               "IMAGEMURL, ESTOQUE, DATACADASTRO, CATEGORIAID) VALUES ('Cocada'," +
+               " 'Cocada 250g', 10.0, 'cocada.jpg', 50, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM PRODUTOS");
+        }
+    }
+}
